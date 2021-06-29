225629c59c02a81667257682293a1c05ca30fce7
alexalok <aleks00799@gmail.com>
2021-06-29 14:03:41 +0300

    Add code

---
+++ LoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore.StructuredLogging.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static IDisposable BeginScopeWithProperty(this ILogger logger, string propertyName, object propertyValue) =>
+            logger.BeginScope(new[] { new KeyValuePair<string, object>(propertyName, propertyValue) });
+    }
+}
---
+++ LoggerScopeBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore.StructuredLogging.Extensions
+{
+    public class LoggerScopeBuilder
+    {
+        readonly List<KeyValuePair<string, object>> _properties = new();
+        readonly ILogger _logger;
+
+        public LoggerScopeBuilder(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public LoggerScopeBuilder WithProperty(string propertyName, object propertyValue)
+        {
+            _properties.Add(new KeyValuePair<string, object>(propertyName, propertyValue));
+            return this;
+        }
+
+        public IDisposable Begin() => _logger.BeginScope(_properties);
+    }
+
+    public static class LoggerScopeBuilderExtensions
+    {
+        public static LoggerScopeBuilder CreateScope(this ILogger logger) => new(logger);
+    }
+}
